77d4fa498443a1587065232ca87fdd0508903bb6
DareSlave96 <dareslave96@mail.com>
2014-08-31 10:31:11 +0100

    Update to a different data structure

--- start.py
+++ start.py
@@ -10,29 +10,25 @@ safeword = "pinapples"
 gender = ""
 dom_gender = ""
 play_list = []
-limits_master = ['anal','pissing yourself','drinking piss','shitting yourself','holding your own shit','eating shit','pain','CBT','humiliation','hidden public','obvious public']
+limits_master = ['anal','pissing yourself','drinking piss','shitting yourself','holding shit','eating shit','pain','CBT','humiliation','hidden public','obvious public']
 items_master = ['small dildo','medium dildo','large dildo','small butt plug','medium butt plug','large butt plug','inflatable butt plug','vibrator','blindfold','gag','collar','chastity belt','daiper','paddle','wooden spoon','clothes peg','clover clamp','tweezer clamp']
 clothes_master = ['male boxer shorts','mens briefs','a skirt','a dress','a blouse','a bra','ladies briefs']
 
 ### Variables to store dom(me)s mood and desires
 des_anal = 0
-des_pissSelf = 0
-des_pissDrink = 0
-des_shitSelf = 0
-des_shitHold = 0
-des_shitEat = 0
+des_piss = 0
+des_shit = 0
 des_pain = 0
 des_CBT = 0
 des_humiliation = 0
-des_hPublic = 0
-des_oPublic = 0
+des_public = 0
 
 des_play = 50;
 
 pnts = 25
 
 ### Assign the current version of the program
-current_version = '2'
+current_version = '3'
 
 ### Main function
 def main():
@@ -295,16 +291,12 @@ def save_prefs ():
 		f.write( dom_gender + "\n")
 		f.write( gender + "\n")
 		f.write( str(des_anal) + "\n")
-		f.write( str(des_pissSelf) + "\n")
-		f.write( str(des_pissDrink) + "\n")
-		f.write( str(des_shitSelf) + "\n")
-		f.write( str(des_shitHold) + "\n")
-		f.write( str(des_shitEat) + "\n")
+		f.write( str(des_piss) + "\n")
+		f.write( str(des_shit) + "\n")
 		f.write( str(des_pain) + "\n")
 		f.write( str(des_CBT) + "\n")
 		f.write( str(des_humiliation) + "\n")
-		f.write( str(des_hPublic) + "\n")
-		f.write( str(des_oPublic) + "\n")
+		f.write( str(des_public) + "\n")
 		f.write( str(des_play) + "\n")
 		f.write( str(pnts) + "\n")
 ### load general preferences
@@ -316,47 +308,237 @@ def load_prefs ():
 			gender = f.readline().rstrip('\n')
 			global des_anal
 			des_anal = int( f.readline().rstrip( '\n' ) )
-			des_pissSelf = int( f.readline().rstrip( '\n' ) )
-			des_pissDrink = int( f.readline().rstrip( '\n' ) )
-			des_shitSelf = int( f.readline().rstrip( '\n' ) )
-			des_shitHold = int( f.readline().rstrip( '\n' ) )
-			des_shitEat = int( f.readline().rstrip( '\n' ) )
+			des_piss = int( f.readline().rstrip( '\n' ) )
+			des_shit = int( f.readline().rstrip( '\n' ) )
 			des_pain = int( f.readline().rstrip( '\n' ) )
 			des_CBT = int( f.readline().rstrip( '\n' ) )
 			des_humiliation = int( f.readline().rstrip( '\n' ) )
-			des_hPublic = int( f.readline().rstrip( '\n' ) )
-			des_oPublic = int( f.readline().rstrip( '\n' ) )
+			des_public = int( f.readline().rstrip( '\n' ) )
 			des_play = int( f.readline().rstrip( '\n' ) )
 			pnts = int( f.readline().rstrip( '\n' ) )
 ### Make a list of all play phrases
 def create_play_list():
-	global limits_master
+	if not 'anal' in limits:
+		with open('data/anal.txt') as f:
+			global instr_anal
+			instr_anal = check_lines( f )
+	if not 'pissing yourself' in limits:
+		with open('data/pissSelf.txt') as f:
+			global instr_piss
+			instr_piss = check_lines( f )
+	if not 'drinking piss' in limits:
+		with open('data/pissDrink.txt') as f:
+			global instr_piss
+			for a in check_lines( f ):
+				instr_piss.append( a )
+	if not 'shitting yourself' in limits:
+		with open('data/shitSelf.txt') as f:
+			global instr_shit
+			instr_shit = check_lines( f )
+	if not 'holding shit' in limits:
+		with open('data/shitHold.txt') as f:
+			global instr_shit
+			for a in check_lines( f ):
+				instr_shit.append( a )
+	if not 'eating shit' in limits:
+		with open('data/shitEat.txt') as f:
+			global instr_shit
+			for a in check_lines( f ):
+				instr_shit.append( a )
+	if not 'pain' in limits:
+		with open('data/pain.txt') as f:
+			global instr_pain
+			instr_pain = check_lines( f ) 
+	if not 'CBT' in limits:
+		with open('data/CBT.txt') as f:
+			global instr_CBT
+			instr_CBT = check_lines( f ) 
+	if not 'humiliation' in limits:
+		with open('data/humiliation.txt') as f:
+			global instr_humiliation
+			instr_humiliation = check_lines( f ) 
+	if not 'hidden public' in limits:
+		with open('data/hPublic.txt') as f:
+			global instr_Public
+			instr_public = check_lines( f ) 
+	if not 'obvious public' in limits:
+		with open('data/oPublic.txt') as f:
+			global instr_public
+			for a in check_lines( f ):
+				instr_public.append( a )
+def check_lines( a ):
 	global play_list
-	play_list = []
-	for a in limits_master:
-		if ( a in limits ) == False:
-			with open('data/' + a + '.txt') as f:
-				for line in f:
-					play_list.append( line.rstrip( '\n' ) )
-			for b in items:
-				with open('data/' + a + '_' + b + '.txt') as f:
-					for line in f:
-						play_list.append( line.rstrip( '\n' ) )
-### Create all of the data files.
-def create( y, z ):
-	import math
-	for a in limits_master:
-		f = open('data/' + a + '.txt', 'w')
-		x = 0
-		while x < z:
-			f.write( ('0123456789'*(math.floor(y/10))) + '\n')
-			x = x + 1
-		for b in items_master:
-			f = open('data/' + a + '_' + b + '.txt', 'w')
+	a = a.readlines()
+	print(a)
+	thing = []
+	for line in a:
+		print(line)
+		mod = line.rstrip( '\n' )
+		line_list = mod.split()
+		write = True
+		if '**limit' in line_list or '**toy' in line_list or '**clothing' in line_list or '**sub_gender' in play_list or '**dom_gender' in play_list:
+			print('stuff')
+			h = 0
+			while h < len(line_list):
+				if '**' in line_list[h]:
+					if line_list[h] == '**limit':
+						print('limits')
+						if line_list[h+1] == 'anal':
+							if not 'anal' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'pissSelf':
+							if not 'pissing yourself' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'pissDrink':
+							if not 'drinking piss' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'shitSelf':
+							if not 'shitting yourself' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'shitHold':
+							if not 'holing shit' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'shitEat':
+							print('eating shit')
+							if not 'eating shit' in limits: 
+								bananaa = 0
+								print('added')
+							else: 
+								write = False
+								print('not added')
+						elif line_list[h+1] == 'pain':
+							if not 'pain' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'CBT':
+							if not 'CBT' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'humiliation':
+							if not 'humiliation' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'hPublic':
+							if not 'hidden public' in limits: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'oPublic':
+							if not 'obvious public' in limits: bananaa = 0
+							else: write = False
+						else:
+							print('no valid limit')
+							write = False
+					elif line_list[h] == '**toy':
+						if line_list[h+1] == 'smlDildo':
+							if 'small dildo' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'medDildo':
+							if 'medium dildo' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'lrgDildo':
+							if 'large dildo' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'smlButtPlug':
+							if 'small butt plug' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'medButtPlug':
+							if 'medium butt plug' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'lrgButtPlug':
+							if 'large butt plug' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'infButtPlug':
+							if 'inflatable butt plugg' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'vibrator':
+							if 'vibrator' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'blindfold':
+							if 'blindfold' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'gag':
+							if 'gag' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'collar':
+							if 'collar' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'chastity':
+							if 'chastity belt' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'daiper':
+							if 'daiper' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'paddle':
+							if 'paddle' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'spoon':
+							if 'wooden spoon' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'clothesPeg':
+							if 'clothes peg' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'cloverClamp':
+							if 'clover clamp' in items: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'tweezerClamp':
+							if 'medium dildo' in items: bananaa = 0
+							else: write = False
+						else:
+							write = False;
+					elif line_list[h] == '**clothing':
+						if line_list[h+1] == 'mBoxers':
+							if 'male boxer shorts' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'mBriefs':
+							if 'mens briefs' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'fSkirt':
+							if 'a skirt' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'fDress':
+							if 'a dress' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'fBlouse':
+							if 'a blouse' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'fBra':
+							if 'a bra' in clothes: bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'fBriefs':
+							if 'ladies briefs' in clothes: bananaa = 0
+							else: write = False
+						else:
+							write = False
+					elif line_list[h] == '**sub_gender':
+						if line_list[h+1] == 'male':
+							if gender == 'male': bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'female':
+							if gender == 'female': bananaa = 0
+							else: write = False
+						else:
+							write = False
+					elif line_list[h] == '**dom_gender':
+						if line_list[h+1] == 'male':
+							if dom_gender == 'male': bananaa = 0
+							else: write = False
+						elif line_list[h+1] == 'female':
+							if dom_gender == 'female': bananaa = 0
+							else: write = False
+						else:
+							write = False
+					else:
+						write = False
+						print('no valid tag')
+					line_list.remove( line_list[h] )
+					line_list.remove( line_list[h] )
+				else:
+					h = h + 1
+		if write == True:
 			x = 0
-			while x < z:
-				f.write( ('0123456789'*(math.floor(y/10))) + '\n')
+			mod = ''
+			while x < len(line_list):
+				mod = mod + line_list[x] + ' '
 				x = x + 1
+			thing.append( mod )
+			play_list.append( mod )
+	return thing		
 			
 ### Repeating function
 def repeat():
